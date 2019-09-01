ab0baf3a50544b901cd93d653598dd209a696dc0
Andreas Thor <dr.andreas.thor@googlemail.com>
2019-09-01 22:34:49 +0200

    fragen begonnen

--- cp/Manifest_Generator.php
+++ cp/Manifest_Generator.php
@@ -71,11 +71,58 @@ foreach ($content['chapter'] as $chapter) {
 	
 	file_put_contents ( $directory . '/imsmanifest.xml', $dom->saveXML());
 	
-	foreach (['accordion.js', 'ims_xml.xsd', 'imscp_v1p1.xsd', 'imsmd_v1p2p2.xsd', 'page.css'] as $staticfile) {
-		copy('files/' . $staticfile , $directory . '/' . $staticfile);
+// 	foreach (['accordion.js', 'ims_xml.xsd', 'imscp_v1p1.xsd', 'imsmd_v1p2p2.xsd', 'page.css'] as $staticfile) {
+// 		copy('files/' . $staticfile , $directory . '/' . $staticfile);
+// 	}
+	
+	recurseCopy('files', $directory);
+	
+	
+	// Get real path for our folder
+	echo $rootPath = realpath($directory);
+	
+	// Initialize archive object
+	$zip = new ZipArchive();
+	$zip->open($directory . '.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE );
+	
+	// Create recursive directory iterator
+	/** @var SplFileInfo[] $files */
+	$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($rootPath), RecursiveIteratorIterator::LEAVES_ONLY);
+	
+	foreach ($files as $name => $file)
+	{
+		// Skip directories (they would be added automatically)
+		if (!$file->isDir())
+		{
+			// Get real and relative path for current file
+			$filePath = $file->getRealPath();
+			$relativePath = substr($filePath, strlen($rootPath) + 1);
+			
+			// Add current file to archive
+			$zip->addFile($filePath, $relativePath);
+		}
 	}
 	
+	// Zip archive will be created only after closing object
+	$zip->close();
 
+}
+
+
+function recurseCopy($src, $dst)
+{
+	$dir = opendir($src);
+	if (!is_dir($dst)) mkdir($dst);
+	while(false !== ( $file = readdir($dir)) ) {
+		if (( $file != '.' ) && ( $file != '..' )) {
+			if ( is_dir($src . '/' . $file) ) {
+				recurseCopy($src . '/' . $file, $dst . '/' . $file);
+			} else {
+				copy($src . '/' . $file,$dst . '/' . $file);
+			}
+		}
+	}
+	closedir($dir);
 }
 
 ?>
