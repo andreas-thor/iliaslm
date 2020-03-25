2f3cd2974e6bcaffdaff1304d4f89d539bca8bfc
Thor <athor@htwk-leipzig.de>
2020-03-25 01:14:03 +0100

    java datentypen und operatoren

--- run.bat
+++ run.bat
@@ -2,3 +2,4 @@ cd /D "%~dp0"
 cd cp
 REM "C:\Program Files\php\php.exe" Content_Package_Generator.php ..\learnmodules\dbs.json ..\tmp https://www1.hft-leipzig.de/thor/dbs/
 "C:\Program Files\php\php.exe" Content_Package_Generator.php ..\learnmodules\java.json ..\tmp https://www1.hft-leipzig.de/thor/java/
+
