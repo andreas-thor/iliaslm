5b304561f13a516b2d44eb14dfaef8e5455ed084
Thor <athor@htwk-leipzig.de>
2020-10-05 16:38:26 +0200

    dbs

--- run.bat
+++ run.bat
@@ -1,8 +1,5 @@
 cd /D "%~dp0"
 cd cp
 
-        
-
-REM "C:\Program Files\php\php.exe" Content_Package_Generator.php ..\learnmodules\dbs.json ..\tmp https://www1.hft-leipzig.de/thor/dbs/
-REM "C:\Program Files\php\php.exe" Content_Package_Generator.php ..\learnmodules\java.json ..\tmp https://www1.hft-leipzig.de/thor/java/
-"C:\Program Files\php\php.exe" Content_Package_Generator.php "C:\Lehre\VL_Prog\lernmodul\java.json" ..\tmp
+REM "C:\Program Files\php\php.exe" Content_Package_Generator.php "C:\Lehre\VL_Prog\lernmodul\java.json" ..\tmp
+"C:\Program Files\php\php.exe" Content_Package_Generator.php "C:\Lehre\VL_DBS\lernmodul\dbs.json" ..\tmp
