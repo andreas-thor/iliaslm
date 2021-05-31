51b4c33d083561f1dc5931aff767efd49ce2c77a
Andreas Thor <andreas.thor@htwk-leipzig.de>
2021-05-31 12:41:28 +0200

    update token

--- run.bat
+++ run.bat
@@ -3,3 +3,5 @@ cd cp
 
 "C:\Program Files\php\php.exe" Content_Package_Generator.php "C:\Lehre\VL_Prog\lernmodul\java.json" ..\tmp
 REM "C:\Program Files\php\php.exe" Content_Package_Generator.php "C:\Lehre\VL_DBS\lernmodul\dbs.json" ..\tmp
+
+REM für commit
